de790a55a6359731b73b110aed289c5fdc98c2a7
Olusola Afikode <afikodeolaitan@gmail.com>
2020-11-28 08:19:14 +0100

    Work on Product Page

--- src/Home.js
+++ src/Home.js
@@ -13,14 +13,54 @@ function Home() {
 				/>
 
 				<div className="home__row">
-					<Product />
-					{/* Product */}
+					<Product
+						id="12321341" 
+						title="The lean Startup: How Constant Innovation Creates Radically Successful Businesses Paperback" 
+						price={29.99} 
+						image="https://gornapolkatvn.files.wordpress.com/2013/03/metoda-lean-startup.jpg"  
+						rating={5} 
+					/>
+					<Product 
+						id="45865514"
+						title="The Universal Design Kitchen Product"
+						price={149.99}
+						image="https://th.bing.com/th/id/OIP.qB5qqxzKF-b3rJLPr0N-XgHaHa?pid=Api&w=640&h=640&rs=1"
+						rating={4}
+					/>
 				</div>
 
 				<div className="home__row">
-					{/* Product */}
-					{/* Product */}
-					{/* Product */}
+					<Product
+						id="56088885" 
+						title="Samsung LC49RG90SSUXEN 49' Curved LED Gaming Monitor"
+						price={199.99}
+						image="https://image.ceneostatic.pl/data/products/81056986/4fa017ac-a9ef-4d33-adfe-570a7805af35_i-samsung-49-crg90-lc49rg90ssuxen.jpg?=f4b82"
+						rating={3}
+					/>
+					<Product 
+						id="23445930"
+						title="Amazon Echo (3rd generation) | Smart speaker with Alexa, Charcoal Fabric"
+						price={98.99}
+						image="https://sm.pcmag.com/t/pcmag_uk/review/a/amazon-ech/amazon-echo-3rd-generation_7w88.1200.jpg"
+						rating={5}
+					/>
+					<Product 
+						id="6667788"
+						title="New Apple iPad Pro (12.9-inch, Wi-Fi, 128GB) - Silver (4th Generation)"
+						price={589.99}
+						image="https://cdn.mobilesyrup.com/wp-content/uploads/2020/03/ipad-pro-2020-header-scaled.jpg"
+						rating={4}
+					/>
+				</div>
+
+				<div className="home__row">
+					<Product
+						id="44455566"
+						title= "Play Station 5: Have a thrilling experience"
+						price={699.99}
+						image="https://i.ytimg.com/vi/--fXXoZmjnw/maxresdefault.jpg"
+						rating={5} 
+					/>
 				</div>
 			</div>
 		</div>
--- src/Product.js
+++ src/Product.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import './Product.css';
 
-function Product() {
+function Product({ title, image, price, rating }) {
 	return (
 		<div className="product">
 			<div className="product__info">
-				<p> The lean startup:</p>
+				<p> {title}</p>
 				<p className="product__price">
 					<small>$</small>
-					<strong>19.99</strong>
+					<strong>{price}</strong>
 				</p>
 				<div className="product__rating">
-					<p>⭐</p>
+					{Array(rating)
+						.fill()
+						.map((_, i) => (
+							<p>⭐</p>
+					))}
 				</div>
 			</div>
 			<img 
-				src="https://gornapolkatvn.files.wordpress.com/2013/03/metoda-lean-startup.jpg"
+				src={image}
 				alt="" 
 			/>
 
@@ -25,3 +29,4 @@ function Product() {
 }
 
 export default Product;
+
