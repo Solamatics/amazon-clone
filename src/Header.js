b7e512b311e4403ffc06629e436d4df11a7453d4
Olusola Afikode <afikodeolaitan@gmail.com>
2020-12-31 16:26:30 +0100

    Complete User authentication

--- src/Header.js
+++ src/Header.js
@@ -4,9 +4,16 @@ import SearchIcon from '@material-ui/icons/Search';
 import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
 import { Link } from 'react-router-dom';
 import { useStateValue } from './StateProvider';
+import { auth } from './firebase';
 
 function Header() {
-	const [{ basket }, dispatch] = useStateValue();
+	const [{ basket, user }, dispatch] = useStateValue();
+
+	const handleAuthentication = () => {
+		if(user) {
+			auth.signOut();
+		}
+	}
 
 	return (
 
@@ -31,10 +38,10 @@ function Header() {
 			</div>
 
 			<div className='header_nav'>
-				<Link style={{textDecoration: 'none'}} to="/login">
-					<div className='header_option'>
+				<Link style={{textDecoration: 'none'}} to={!user && "/login"}>
+					<div onClick={handleAuthentication} className='header_option'>
 						<span className="header__optionLineOne">Hello Guest</span>
-						<span className="header__optionLineTwo">Sign In</span>
+						<span className="header__optionLineTwo">{user ? 'Sign Out' : 'Sign In'}</span>
 					</div>
 				</Link>
 
